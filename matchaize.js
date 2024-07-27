d4fff33dab320aa4cf81998f1090910c54f1872c
Pradeep Gowda <pradeep@btbytes.com>
2024-07-27 14:52:29 -0400

    reformat

--- matchaize.js
+++ matchaize.js
@@ -2,19 +2,12 @@
   const $$ = (selector) => document.querySelectorAll(selector);
   const createElement = (tagName, properties) =>
     Object.assign(document.createElement(tagName), properties);
-
-  // Remove all CSS stylesheets, external and internal
   $$('link[rel="stylesheet"],style').forEach((el) => el.remove());
-
-  // Remove all inline styles
   $$("*").forEach((el) => (el.style = ""));
-
   const linkElm = createElement("link", {
     rel: "stylesheet",
     href: "https://matcha.mizu.sh/matcha.css",
   });
-
-  // Add water.css and responsive viewport (if necessary)
   document.head.append(
     linkElm,
     !$$('meta[name="viewport"]').length &&
